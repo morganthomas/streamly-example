4ff77832e449fa0e7112867e99e47fcf0c3322d1
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2018-07-13 10:32:45 -0600

    fix

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,21 @@
-{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE TupleSections, FlexibleContexts #-}
 
 
 module Main where
 
 
-import Control.Concurrent
+import Control.Monad.Catch         (MonadThrow)
+import Control.Monad.Trans.Control (MonadBaseControl)
 import Control.Concurrent.Async
-import Control.Concurrent.MVar
 import Control.Monad.IO.Class
+import Data.Foldable
 import Data.Functor
+import qualified Control.Foldl as L
 import qualified Data.Map         as M
 import Streamly
 import Streamly.Prelude as S
+import UnliftIO
+import UnliftIO.Concurrent
 
 
 testData :: [Int]
@@ -34,12 +38,23 @@ testStream = do
 
 main :: IO ()
 main = do
-  s <- testStream
-  void . forkIO . printStream . adapt $ fmap ("R",) s
-  void . forkIO . printStream . adapt $ fmap ("G",) s
-  void . forkIO . printStream . adapt $ fmap ("B",) s
+  [sR,sG,sB] <- testStream >>= cloneStream 3
+  void . forkIO . printStream . adapt $ fmap ("R",) sR
+  void . forkIO . printStream . adapt $ fmap ("G",) sG
+  void . forkIO . printStream . adapt $ fmap ("B",) sB
   threadDelay 5000000
 
 
 printStream :: Show a => SerialT IO a -> IO ()
 printStream = S.mapM_ (liftIO . putStrLn . show)
+
+
+-- A stream of actions should be considered single-consumer because each action result
+-- will be given to one consumer only. With cloneStream you can create multiple distinct
+-- copies of a stream to pass to multiple consumers.
+cloneStream :: (MonadUnliftIO m, MonadBaseControl IO m, MonadThrow m)
+  => Int -> SerialT m a ->  m [SerialT m a]
+cloneStream n s = do
+  vars <- Prelude.sequence (replicate n newEmptyMVar)
+  void . forkIO $ S.mapM_ (\x -> forM_ vars (\v -> putMVar v x)) s
+  return $ fmap (\v -> serially . S.repeatM $ takeMVar v) vars
